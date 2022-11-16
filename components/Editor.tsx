ad32e6e1ddf4cf3eb8814b440b252d863bf8e44c
sh1okoh <kooooo.0719task@gmail.com>
2022-11-16 23:56:51 +0900

    tooltipでの編集をできるようにした

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,10 +1,26 @@
-import React, { useState } from 'react';
-import 'react-quill/dist/quill.snow.css';
-import ReactQuill from "react-quill";
+import React, { useLayoutEffect, useState } from 'react';
+// import 'react-quill/dist/quill.snow.css';
+import 'react-quill/dist/quill.bubble.css'
+import ReactQuill, { Quill } from "react-quill";
+import "react-quill/dist/quill.bubble.css";
+
+const BubbleTheme = Quill.import("themes/bubble");
+
+class ExtendBubbleTheme extends BubbleTheme {
+  constructor(quill:any, options:any) {
+    super(quill, options);
+
+    quill.on("selection-change", (range:any) => {
+      if (range) {
+        quill.theme.tooltip.show();
+        quill.theme.tooltip.position(quill.getBounds(range));
+      }
+    });
+  }
+}
 
 export default function Editor() {
   const [value, setValue] = useState('');
-  console.log('value', value);
 
   const modules = {
     toolbar: [
@@ -21,8 +37,17 @@ export default function Editor() {
     'bold', 'italic', 'underline', 'strike', 'blockquote',
     'list', 'bullet', 'indent',
     'link', 'image'
-  ]
+  ];
 
-  return <ReactQuill theme="snow" modules={modules} formats={formats} onChange={setValue}/>
-  // return <>div</>
+  useLayoutEffect(() => {
+    Quill.register("themes/bubble", ExtendBubbleTheme);
+  })
+
+  return <ReactQuill
+  theme="bubble"
+  placeholder="Compose an epic..."
+  modules={modules}
+  onChange={setValue}
+  formats={formats}
+/>
 }
